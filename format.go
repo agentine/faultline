950615341773d56f64459c8944b9a45964637567
Matth <matth@mtingers.com>
2026-03-13 12:03:46 -0500

    add package documentation to format.go

--- format.go
+++ format.go
@@ -1,1 +1,10 @@
+// Package faultline provides error handling primitives with stack traces.
+//
+// faultline is a drop-in replacement for github.com/pkg/errors that fixes
+// the compatibility issue between Cause() and errors.Is/errors.As by
+// implementing both Cause() and Unwrap() on all wrapper types.
+//
+// The formatting verbs are implemented directly on each error type
+// (fundamental, withStack, withMessage, withCause) in their respective
+// source files (errors.go, wrap.go).
 package faultline
